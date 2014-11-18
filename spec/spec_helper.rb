830a2f7afbd2febafab7190f1d43c07aef0ee026
ajwhi7 <adamw100@hotmail.com>
2014-11-18 16:07:49 +1100

    Finish Static pages examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'capybara'
+require 'capybara/dsl'
+RSpec.configure do |config|
+
+
+
+  config.include Capybara::DSL
+end
+
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
