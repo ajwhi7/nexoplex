6dc7ce73d32282a3809e8e70defd978f29a57bfc
ajwhi7 <adamw100@hotmail.com>
2014-11-19 12:16:47 +1100

    Page 141

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,16 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Nexoplex')
     end
-    it "should have the title 'Home'" do
-  visit '/static_pages/home'
-  expect(page).to have_title("Nexoplex | Home")
-end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Nexoplex")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
   end
   describe "Help page" do
 
